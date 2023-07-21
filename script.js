e19aba9e71c1ecda8d17ed9ba4417376fdc90659
ArokiaSamy45 <dossarokia45@gmail.com>
2023-07-21 12:31:07 +0530

    final commit

--- script.js
+++ script.js
@@ -1,51 +1,108 @@
-var con = document.createElement('div');
-        con.classList.add('Heading');
-        var content = document.querySelector('body').append(con);
-        var re = document.querySelector('div') 
+const container = document.createElement("div");
+container.classList.add("pokemon-container");
+document.body.appendChild(container);
 
+function createHeader() {
+  const header = document.createElement("header");
+  header.classList.add("navbar");
 
+  const title = document.createElement("h1");
+  title.classList.add("navbar_title");
+  title.textContent = "Pokemon API";
 
-async function api(){
-    
-        const promises = [];
-        for(let i = 1; i <=50; i++) {
-            var url = fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-            out = await url
-            prom = out.json()
-            data = await prom
-            promises.push(data);
-        }
-        Promise.all(promises).then((results) => {
-            const pokemon = results.map((data) => ({
-                name: data.name,
-                image: data.sprites['front_default'],
-                ability: data.abilities.map((ability)=> ability.ability.name).join(', '),
-                move: data.moves.map((move)=> move.move.name).join(', '),
-                weight: data.weight,
-            }));
-            displayPokemon(pokemon);
-        });
-            
-       const displayPokemon = (pokemon) => {
-        console.log(pokemon);
-    const pokemonString = pokemon.map (pokeman => 
-        `<li class="card">
-        <h2 class="card_name">Name: ${pokeman.name}</h2>
-    <img class="card_image" src="${pokeman.image}"/>
-    <p class="card_ability">Ability: ${pokeman.ability}</p>
-    <p class="card_weight">Weight: ${pokeman.weight}</p>
-    </li>`).join(''); 
-    re.innerHTML = pokemonString;
-
-    };
-        
-        
-      
-        
-    
-    
-
+  header.appendChild(title);
+  document.body.insertBefore(header, container);
 }
-api()
 
-{/* <p class="card_move">Move: ${pokeman.move}</p> */}
+async function api(page) {
+  try {
+    const promises = [];
+    const offset = (page - 1) * 10;
+
+    for (let i = 1 + offset; i <= 10 + offset; i++) {
+      const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      promises.push(data);
+    }
+
+    Promise.all(promises).then((results) => {
+      const pokemon = results.map((data) => ({
+        name: data.name,
+        image: data.sprites["front_default"],
+        ability: data.abilities
+          .map((ability) => ability.ability.name)
+          .join(", "),
+        move: data.moves.map((move) => move.move.name).join(", "),
+        weight: data.weight,
+      }));
+      displayPokemon(pokemon);
+    });
+  } catch (error) {
+    console.error("Error fetching Pokemon data:", error);
+  }
+}
+
+const displayPokemon = (pokemon) => {
+  container.innerHTML = "";
+
+  pokemon.forEach((pokemon) => {
+    const card = document.createElement("li");
+    card.classList.add("card");
+
+    const nameElement = document.createElement("h2");
+    nameElement.classList.add("card_name");
+    nameElement.textContent = `Name: ${pokemon.name}`;
+    card.appendChild(nameElement);
+
+    const imageElement = document.createElement("img");
+    imageElement.classList.add("card_image");
+    imageElement.src = pokemon.image;
+    card.appendChild(imageElement);
+
+    const abilityElement = document.createElement("p");
+    abilityElement.classList.add("card_ability");
+    abilityElement.textContent = `Ability: ${pokemon.ability}`;
+    card.appendChild(abilityElement);
+
+    const weightElement = document.createElement("p");
+    weightElement.classList.add("card_weight");
+    weightElement.textContent = `Weight: ${pokemon.weight}`;
+    card.appendChild(weightElement);
+
+    container.appendChild(card);
+  });
+};
+
+let currentPage = 1;
+
+document.addEventListener("DOMContentLoaded", () => {
+  createHeader();
+
+  api(currentPage);
+
+  const nextBtn = document.createElement("button");
+  nextBtn.textContent = "Next";
+  nextBtn.id = "nextBtn";
+  nextBtn.addEventListener("click", () => {
+    currentPage++;
+    api(currentPage);
+  });
+
+  const prevBtn = document.createElement("button");
+  prevBtn.textContent = "Previous";
+  prevBtn.id = "prevBtn";
+  prevBtn.addEventListener("click", () => {
+    if (currentPage > 1) {
+      currentPage--;
+      api(currentPage);
+    }
+  });
+
+  const paginationDiv = document.createElement("div");
+  paginationDiv.classList.add("pagination");
+  paginationDiv.appendChild(prevBtn);
+  paginationDiv.appendChild(nextBtn);
+
+  document.body.appendChild(paginationDiv);
+});
